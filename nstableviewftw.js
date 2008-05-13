b1eff80e4e6e9444df84339480431a12397b7880
Joannou Ng <mundane@blackbean.local>
2008-05-13 16:36:19 -0400

    Namespaced render function for AJAX compatibility

--- nstableviewftw.js
+++ nstableviewftw.js
@@ -1,72 +1,76 @@
-$$('table.NSTableViewFTW').each(function(aTable) {
-    // Set <thead> width and <tbody> width & height
-    var aTableWidth = aTable.getStyle('width').slice(0, -2);
-    var aTBody = aTable.select('tbody').first();
+var NSTableViewFTW = {
+    render: function() {
+        $$('table.NSTableViewFTW').each(function(aTable) {
+            // Set <thead> width and <tbody> width & height
+            var aTableWidth = aTable.getStyle('width').slice(0, -2);
+            var aTBody = aTable.select('tbody').first();
 
-    aTable.select('thead').first().setStyle({
-        width: aTableWidth + 'px',
-    });
-
-    aTBody.setStyle({
-        width: aTableWidth + 'px',
-        height: aTable.getStyle('height').slice(0, -2) - 17 + 'px'
-    });
-
-
-
-    // Set <td> widths
-    var allRows = aTable.select('tr');
-
-    allRows.each(function(aRow) {
-        aRow.select('td').each(function(aCell, aColumnIndex) {
-            aCell.addClassName('NSTableViewFTWColumn' + aColumnIndex);
-        });
-    });
-    
-    var firstRowCells = allRows.first().select('td');
-    var columnCount = firstRowCells.size();
-
-    columnCount.times(function(i) {
-        var aColumnOfCells = aTable.select('td.NSTableViewFTWColumn' + i);
-
-        var aCellWidth = (aColumnOfCells.inject(0, function(anAccumulator, aCell) {
-            return anAccumulator + aCell.innerHTML.stripScripts().stripTags().strip().length;
-        }) / allRows.size()).round() * 10;
-        
-        aColumnOfCells.invoke('setStyle', {
-            width: aCellWidth + 'px'
-        });
-
-        if (i) {
-            aColumnOfCells.first().setStyle({
-                width: ++aCellWidth + 'px'
+            aTable.select('thead').first().setStyle({
+                width: aTableWidth + 'px',
             });
-        }
-    });
+
+            aTBody.setStyle({
+                width: aTableWidth + 'px',
+                height: aTable.getStyle('height').slice(0, -2) - 17 + 'px'
+            });
 
 
 
-    // Set <tr> widths
-    var rowWidth = firstRowCells.inject(0, function(anAccumulator, aCell) {
-        return anAccumulator + aCell.getStyle('width').slice(0, -2) * 1;
-    }) + columnCount - 1 + columnCount * 8;
+            // Set <td> widths
+            var allRows = aTable.select('tr');
 
-    allRows.invoke('setStyle', {
-       width: rowWidth + 'px'
-    });
+            allRows.each(function(aRow) {
+                aRow.select('td').each(function(aCell, aColumnIndex) {
+                    aCell.addClassName('NSTableViewFTWColumn' + aColumnIndex);
+                });
+            });
+    
+            var firstRowCells = allRows.first().select('td');
+            var columnCount = firstRowCells.size();
+
+            columnCount.times(function(i) {
+                var aColumnOfCells = aTable.select('td.NSTableViewFTWColumn' + i);
+
+                var aCellWidth = (aColumnOfCells.inject(0, function(anAccumulator, aCell) {
+                    return anAccumulator + aCell.innerHTML.stripScripts().stripTags().strip().length;
+                }) / allRows.size()).round() * 10;
+        
+                aColumnOfCells.invoke('setStyle', {
+                    width: aCellWidth + 'px'
+                });
+
+                if (i) {
+                    aColumnOfCells.first().setStyle({
+                        width: ++aCellWidth + 'px'
+                    });
+                }
+            });
+
+
+
+            // Set <tr> widths
+            var rowWidth = firstRowCells.inject(0, function(anAccumulator, aCell) {
+                return anAccumulator + aCell.getStyle('width').slice(0, -2) * 1;
+            }) + columnCount - 1 + columnCount * 8;
+
+            allRows.invoke('setStyle', {
+               width: rowWidth + 'px'
+            });
     
     
 
-    // Horizontally scroll <thead>'s <tr> and vertically scroll <tbody>'s background image with <tbody>'s <tr>s
-    aTBody.observe('scroll', function(anEvent) {
-        var thisTBody = anEvent.element();
+            // Horizontally scroll <thead>'s <tr> and vertically scroll <tbody>'s background image with <tbody>'s <tr>s
+            aTBody.observe('scroll', function(anEvent) {
+                var thisTBody = anEvent.element();
         
-        thisTBody.up('table.NSTableViewFTW').select('thead tr').first().setStyle({
-            left: '-' + thisTBody.scrollLeft + 'px'
-        });
+                thisTBody.up('table.NSTableViewFTW').select('thead tr').first().setStyle({
+                    left: '-' + thisTBody.scrollLeft + 'px'
+                });
         
-        thisTBody.setStyle({
-            backgroundPosition: '0px -' + thisTBody.scrollTop + 'px'
+                thisTBody.setStyle({
+                    backgroundPosition: '0px -' + thisTBody.scrollTop + 'px'
+                });
+            });
         });
-    });
-});
+    }
+};
